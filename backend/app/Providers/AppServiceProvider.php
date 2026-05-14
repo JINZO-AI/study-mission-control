213f848f4947c2912802cfabfb8758ae16a1001d
Mohamed Jawad Touir <jawadtouir03@gmail.com>
2026-05-14 09:40:40 +0100

    fix: set default string length to 191 to fix MySQL key too long error

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,10 +2,15 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
-    public function boot(): void {}
+
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+    }
 }
